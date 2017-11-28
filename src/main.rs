32edd6599aa50437c6e6475dcb0da1b3fa189da7
luozijun <gnulinux@126.com>
2017-11-28 08:15:41 +0800

    FIX: JPEG MAX SIZE (in pixels)

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,61 @@
-#![allow(dead_code, unused_must_use, non_upper_case_globals, unused_variables, unused_mut, unused_imports)]
 
 extern crate image;
 
-use std::fs::File;
-use std::time::{Duration, Instant};
-use std::io::Cursor;
-use std::io::Write;
+use std::time;
 
-// use image::GenericImage;
-
-const BOX_WIDTH: usize  = 512;
-const BOX_HEIGHT: usize = 512;
-
-const WIDTH: usize  = BOX_WIDTH * 294;
-const HEIGHT: usize = BOX_HEIGHT * 12;
-const SIZE: usize   = WIDTH * HEIGHT;
+use image::GenericImage;
+use image::Pixel;
+use image::ImageBuffer;
 
 
-fn read_box(x: u32, y: u32) -> Vec<u8> {
-    let dim = image::open("data/l5_2_45.jpg").unwrap();
-    dim.raw_pixels()
-}
+// 486 392
+const WIDTH: u32  = 293*512 + 486;
+const HEIGHT: u32 = 11*512 + 392;
+const SIZE: usize = (WIDTH * HEIGHT) as usize;
 
-fn put_box(x: u32, y: u32, pixels: &mut [u8]) {
-    let _pixels = read_box(x+1, y+1);
+// JPEG Max size (in pixels): 65,535 x 65,535 (width x height)
 
-    let sx = BOX_WIDTH * x as usize;
-    let sy = BOX_HEIGHT * y as usize;
+fn worker(){
+    println!("Sample: {} * {} = {} pixels", WIDTH, HEIGHT, SIZE);
 
-    for col in 0..BOX_WIDTH {
-        for row in 0..BOX_HEIGHT {
-            let idx = (sx+col) * (sy+row);
-            pixels[idx] = _pixels[col*row];
-            pixels[idx+1] = _pixels[col*row+1];
-            pixels[idx+2] = _pixels[col*row+2];
+    let now = time::Instant::now();
+    for x in 0..294 {
+        let mut img = ImageBuffer::new(if x == 293 { 486 } else { 512 }, HEIGHT);
+        img.put_pixel(0, 0, image::Rgb([0, 0, 0]));
+        for y in 0..12 {
+            let fname = format!("data/l5_{}_{}.jpg", y+1, x+1);
+            let dim = image::open(&fname).unwrap();
+            dim.pixels().for_each(|(w, h, pixel)|{
+                let rgb_pixel = pixel.to_rgb();
+                img.put_pixel(w, y*512+h, rgb_pixel);
+            });
         }
+        println!("X: {:?}", x);
+        img.save(format!("result/{}.png", x+1)).unwrap();
     }
+    println!("\nduration: {:?}", now.elapsed());
+
+    let now2 = time::Instant::now();
+    let mut img = ImageBuffer::new(WIDTH, HEIGHT);
+    img.put_pixel(0, 0, image::Rgb([0, 0, 0]));
+    for y in 0..294 {
+        let fname = format!("result/{}.png", y + 1);
+        let dim = image::open(&fname).unwrap();
+        dim.pixels().for_each(|(w, h, pixel)|{
+            let rgb_pixel = pixel.to_rgb();
+            img.put_pixel(y*512+w, h, rgb_pixel);
+        });
+    }
+    println!("\nduration: {:?}", now2.elapsed());
+
+    println!(":: Merge ...");
+    let now3 = time::Instant::now();
+    img.save("output.png").unwrap();
+    println!("\nduration: {:?}", now3.elapsed());
+
+    println!("\n\nDONE\n\n");
 }
 
 fn main (){
-    let now = Instant::now();
-
-    let mut file = File::create("output.jpg").unwrap();
-
-    // let mut output = Cursor::new(vec![]);
-    {
-        let mut encoder = image::png::PNGEncoder::new(&mut file);
-        let mut pixels: Vec<u8> = vec![0u8; SIZE*3];
-        for x in 0..294{
-            for y in 0..12 {
-                println!("Process  ./data/l5_{:03}_{:03}.jpg  ZONE: SX: {:06} EX: {:06} SY: {:06} EY: {:06}",
-                    y, x, x*512, x*512+512, y*512, y*512+512
-                 );
-                put_box(x as u32, y as u32, &mut pixels);
-            }
-        }
-        println!("\nEncoder begin ...");
-        let now2 = Instant::now();
-        println!("\nEncoder duration: {:?}", now2.elapsed());
-        encoder.encode(&pixels, WIDTH as u32, HEIGHT as u32, image::ColorType::RGB(1));    
-    }
-    
-    // file.write_all(&output.into_inner());
-    println!("\nDONE: {:?}", now.elapsed());
+    worker();
 }
